acfffe8706bf8d5b416520cc73aa70c7fa779e86
Surajbhati003 <surajbhati003@gmail.com>
2024-12-13 20:19:26 +0530

    added voice background.js

--- background.js
+++ background.js
@@ -2,6 +2,7 @@ console.log("Background script loaded.");
 
 // Function to handle keyboard shortcut and activate the voice assistant
 chrome.commands.onCommand.addListener(function (command) {
+<<<<<<< Updated upstream
   if (command === "toggle-voice-assistant") {
     // Get the active tab and inject script
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
@@ -15,3 +16,18 @@ chrome.commands.onCommand.addListener(function (command) {
     });
   }
 });
+=======
+    if (command === "toggle-voice-assistant") {
+      // Get the active tab and inject script
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        if (tabs.length > 0) {
+          const tabId = tabs[0].id;
+          // Send a message to the content script to toggle the voice assistant
+          chrome.tabs.sendMessage(tabId, { action: "toggle-voice-assistant" });
+        } else {
+          console.error("No active tab found");
+        }
+      });
+    }
+  });
+>>>>>>> Stashed changes
